161d1f587534519222d4a03048c696fce60b1695
Adrian Marin <adrianmarinportillo@gmail.com>
2022-03-12 20:23:08 +0100

    Added controller dates + change port + added cheap cors config

---
+++ src/main/java/com/mangu/congreso_api/repos/FechasViewRepository.java
@@ -0,0 +1,8 @@
+package com.mangu.congreso_api.repos;
+
+import com.mangu.congreso_api.domain.FechasView;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FechasViewRepository extends JpaRepository<FechasView, Long> {
+
+}
---
+++ src/main/java/com/mangu/congreso_api/web/rest/DateController.java
@@ -0,0 +1,32 @@
+package com.mangu.congreso_api.web.rest;
+
+import com.mangu.congreso_api.repos.FechasViewRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v2")
+public class DateController {
+    private static final int DEFAULT_PAGE_SIZE = 50;
+
+    private final Logger logger = LoggerFactory.getLogger(DateController.class);
+
+    private final FechasViewRepository fechasViewRepository;
+
+    public DateController(FechasViewRepository fechasViewRepository) {
+        this.fechasViewRepository = fechasViewRepository;
+    }
+
+    @GetMapping("/dates")
+    public ResponseEntity<List<String>> getAllDates() {
+        return ResponseEntity.ok(
+                this.fechasViewRepository.findAll().stream().map(f -> f.getFecha().toString()).collect(Collectors.toList()));
+    }
+}
